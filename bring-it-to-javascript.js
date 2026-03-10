7c31107a7ef23b33f7b5c405061a94efc9676614
billie89-33 <bowvorn@gmail.com>
2026-03-10 15:41:05 +0700

    Finish JS

---
+++ bring-it-to-javascript.js
@@ -0,0 +1,29 @@
+const ProductTvSamsung = {
+    ProductId : "P001",
+    ProductName : "Samsung Tv 55 inch",
+    price : 5000,
+    Stock : 10
+}
+
+const ProductHeadphones = {
+    ProductId : "P002",
+    ProductName : "Sony Headphones",
+    price : 2000,
+    Stock : 20
+}
+
+const ProductWashingMachine = {
+    ProductId : "P003",
+    ProductName : "Samsung Washing machine",
+    price : 2000,
+    Stock : 5
+}
+
+
+const showcase = [
+  ProductHeadphones.ProductName,
+  ProductTvSamsung.ProductName,
+  ProductWashingMachine.ProductName
+];
+
+console.log(showcase)
