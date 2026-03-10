5676e9ad6cf9a59bf2fc7332afac0e587f5639dd
billie89-33 <bowvorn@gmail.com>
2026-03-10 17:12:33 +0700

    UpdetaJS

--- bring-it-to-javascript.js
+++ bring-it-to-javascript.js
@@ -1,24 +1,33 @@
-const ProductTvSamsung = {
+let cart =[]
+
+let ProductTvSamsung = {
     ProductId : "P001",
     ProductName : "Samsung Tv 55 inch",
     price : 5000,
     Stock : 10
 }
 
-const ProductHeadphones = {
+let ProductHeadphones = {
     ProductId : "P002",
     ProductName : "Sony Headphones",
     price : 2000,
     Stock : 20
 }
 
-const ProductWashingMachine = {
+let ProductWashingMachine = {
     ProductId : "P003",
     ProductName : "Samsung Washing machine",
-    price : 2000,
+    price : 2500,
     Stock : 5
 }
 
+
+
+const products = [
+  ProductTvSamsung,
+  ProductHeadphones,
+  ProductWashingMachine
+];
 
 const showcase = [
   ProductHeadphones.ProductName,
@@ -26,4 +35,25 @@ const showcase = [
   ProductWashingMachine.ProductName
 ];
 
-console.log(showcase)
+//เลือกสินค้า
+function chooseItem(products) {
+    for(let i = 0; i < products.length; i++){
+     console.log( ` ${i+1} ${products[i].ProductName} ${"ราคา"} ${products[i].price}` );
+     
+    }
+} 
+
+
+function addCart(product){
+    cart.push(product);
+    return `${product.ProductName} ${"price"} ${product.price}`;
+}
+
+///โชว์ชื่อสินค้าทั้งหมด
+console.log(showcase.join(","));
+
+//แสดงถ้าเลือกสินค้าชินนี้จะโชว์ราคา
+console.log(addCart(ProductTvSamsung))
+
+///แสดงDetailsสินค้าว่ามีอะไรบ้าง
+chooseItem(products);
